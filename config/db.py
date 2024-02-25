60c11ae8c50cd7f350b6ac0a3a5b71ffdb0116df
lenguyenquocdat1219@gmail.com <lenguyenquocdat1219@gmail.com>
2024-02-25 11:20:55 +0700

    Add env file and change custom port

--- config/db.py
+++ config/db.py
@@ -1,4 +1,11 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-conn = MongoClient(
-    "mongodb+srv://quocdat1203:quocdat1203@db.wwee1za.mongodb.net/demo")
+#Load các biến ở file môi trường
+dotenv_path = Path('.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+# Kết nối mongodb
+conn = MongoClient(os.getenv('MONGO_URL'))
--- index.py
+++ index.py
@@ -1,4 +1,14 @@
+import uvicorn
+import os
+
 from fastapi import FastAPI
 from routes.userRoutes import user
+
 app = FastAPI()
-app.include_router(user)
+
+# sử dụng router
+app.include_router(user)\
+
+# chỉ định cho server chạy trên cổng nào    
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=int(os.getenv('PORT')))
