eec2ddc5443690da1d0ee0a919d3091052efc0d4
lapthuan <lapthuan0805@gmail.com>
2024-03-30 10:49:50 +0700

    aa

--- schemas/thietbiSchemas.py
+++ schemas/thietbiSchemas.py
@@ -25,10 +25,10 @@ def serializeList(entity) -> list:
 
         serialized_dict = {
             **{i: str(a[i]) for i in a if i == '_id'},
-            'ipaddress': {'number': conn.demo.ipaddress.find_one({'_id': ObjectId(ipaddress_id)})['ipaddress']} if ipaddress_id and conn.demo.ipaddress.find_one({'_id': ObjectId(ipaddress_id)}) else None,
-            'vlanims': {'number': conn.demo.vlanims.find_one({'_id': ObjectId(vlanims_id)})['number']} if vlanims_id and conn.demo.vlanims.find_one({'_id': ObjectId(vlanims_id)}) else None,
-            'vlanmytv': {'number': conn.demo.vlanmytv.find_one({'_id': ObjectId(vlanmytv_id)})['number']} if vlanmytv_id and conn.demo.vlanmytv.find_one({'_id': ObjectId(vlanmytv_id)}) else None,
-            'vlannet': {'number': conn.demo.vlanNet.find_one({'_id': ObjectId(vlannet_id)})['number']} if vlannet_id and conn.demo.vlanNet.find_one({'_id': ObjectId(vlannet_id)}) else None,
+            'ipaddress':  conn.demo.ipaddress.find_one({'_id': ObjectId(ipaddress_id)})['ipaddress'] if ipaddress_id and conn.demo.ipaddress.find_one({'_id': ObjectId(ipaddress_id)}) else None,
+            'vlanims': conn.demo.vlanims.find_one({'_id': ObjectId(vlanims_id)})['number'] if vlanims_id and conn.demo.vlanims.find_one({'_id': ObjectId(vlanims_id)}) else None,
+            'vlanmytv': conn.demo.vlanmytv.find_one({'_id': ObjectId(vlanmytv_id)})['number']} if vlanmytv_id and conn.demo.vlanmytv.find_one({'_id': ObjectId(vlanmytv_id)}) else None,
+            'vlannet': conn.demo.vlanNet.find_one({'_id': ObjectId(vlannet_id)})['number'] if vlannet_id and conn.demo.vlanNet.find_one({'_id': ObjectId(vlannet_id)}) else None,
             **{i: a[i] for i in a if i not in ['_id', 'ipaddress', 'vlanims', 'vlanmytv', 'vlannet']}
         }
         serialized_list.append(serialized_dict)
